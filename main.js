cdccf2bcd77129a3e7f425364f64b220a8f8a2f4
Vitoria <ana.schoninger@escola.pr.gov.br>
2026-04-19 10:57:37 -0300

    adicionando mais codigos

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelector(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
-
+        botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
     }
-    console.log();
 }
